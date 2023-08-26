60926320628830de3b46ee4b1b85ee09072f7880
Alex Young <alexyoung23j@gmail.com>
2023-08-25 23:23:49 -0400

    tabs

--- packages/referrals-app/src/pages/components.tsx
+++ packages/referrals-app/src/pages/components.tsx
@@ -2,6 +2,14 @@ import { type NextPage } from 'next';
 import { Button, RButton } from '~/components/ui/button';
 import { Avatar, AvatarImage, AvatarFallback } from '~/components/ui/avatar';
 import { RText } from '~/components/ui/text';
+import Icon from '~/components/ui/icons';
+import {
+	RTabsSection,
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from '~/components/ui/tabs';
 
 const ComponentsPage: NextPage = () => {
 	return (
@@ -23,20 +31,75 @@ const ComponentsPage: NextPage = () => {
 					<RText fontWeight="normal">Normal Text</RText>
 				</div>
 
-				<div className="mb-5">
-					{/* <RButton
-					onClick={() => {
-						console.log('yo');
-					}}
-				>
-					Click Me
-				</RButton> */}
+				<div className="m-top-5 mt-10 flex flex-col gap-3">
+					<RText fontSize="h1">Buttons</RText>
+					<RButton
+						onClick={() => {
+							console.log('yo');
+						}}
+					>
+						Primary
+					</RButton>
+					<RButton
+						onClick={() => {
+							console.log('yo');
+						}}
+						variant="secondary"
+					>
+						Secondary
+					</RButton>
+					<RButton
+						onClick={() => {
+							console.log('yo');
+						}}
+						size="sm"
+					>
+						Small
+					</RButton>
+					<RButton
+						onClick={() => {
+							console.log('yo');
+						}}
+						iconName="pencil"
+					>
+						Icon
+					</RButton>
 				</div>
-				<div className="mb-15">
+				<div className="m-top-5 mt-10 flex flex-col gap-3">
+					<RText fontSize="h1">Avatar</RText>
+
 					<Avatar>
 						<AvatarImage src="https://github.com/shadcn.png" />
 						<AvatarFallback>CN</AvatarFallback>
 					</Avatar>
+				</div>
+				<div className="m-top-5 mt-10 flex flex-col gap-3">
+					<RText fontSize="h1">Icons</RText>
+					<Icon name="pencil" />
+				</div>
+				<div className="m-top-5 mt-10 flex flex-col gap-3">
+					<RText fontSize="h1">Tabs</RText>
+					<RTabsSection
+						tabs={[
+							{
+								label: 'Tab 1',
+								iconName: 'pencil',
+								onTabSelect: (tab) => console.log(tab),
+							},
+							{ label: 'Tab 2' },
+						]}
+						onTabsChange={(tab) => console.log('Changed')}
+					/>
+					<RTabsSection
+						tabs={[
+							{ label: 'Tab with content 1' },
+							{ label: 'Tab with content 2' },
+						]}
+						tabContents={[
+							<div key="1">Content for Tab 1</div>,
+							<div key="2">Content for Tab 2</div>,
+						]}
+					/>
 				</div>
 			</div>
 			<div className="bora"></div>
