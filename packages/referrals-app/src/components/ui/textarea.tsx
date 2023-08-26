716a73be9d33785be66e1767b87417d0e5190a31
Bora Yuksel <borayuksel1903@gmail.com>
2023-08-25 21:12:43 -0700

    component kit stuff

--- packages/referrals-app/src/components/ui/textarea.tsx
+++ packages/referrals-app/src/components/ui/textarea.tsx
@@ -1,24 +1,40 @@
-import * as React from "react"
+import * as React from 'react';
+import {Label} from './label';
 
-import { cn } from "src/lib/utils"
+import { cn } from 'src/lib/utils';
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <textarea
-        className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Textarea.displayName = "Textarea"
+	({ className, ...props }, ref) => {
+		return (
+			<textarea
+				className={cn(
+					'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+					className
+				)}
+				ref={ref}
+				{...props}
+			/>
+		);
+	}
+);
+Textarea.displayName = 'Textarea';
 
-export { Textarea }
+interface RTextareaProps extends TextareaProps {
+	label?: string
+	copyEnabled?: boolean
+}
+
+export const RTextarea = ({label, copyEnabled, ...props}: RTextareaProps) => {
+	return (
+		<div>
+			{label && <Label>{label}</Label>}
+			<Textarea {...props} />
+		</div>
+
+	);
+};
+
+export { Textarea };
--- packages/referrals-app/src/pages/components.tsx
+++ packages/referrals-app/src/pages/components.tsx
@@ -3,27 +3,54 @@ import { Button, RButton } from '~/components/ui/button';
 import { Avatar, AvatarImage, AvatarFallback } from '~/components/ui/avatar';
 import { Separator } from '~/components/ui/separator';
 import {Label} from '~/components/ui/label';
+import { Switch } from '~/components/ui/switch';
+import { Input, RInput } from '~/components/ui/input';
+import { RTextarea } from '~/components/ui/textarea';
 
 const ComponentsPage: NextPage = () => {
 	return (
-		<div>
-			<div className=''>
-				<Label className='mb-5 mt-2'>Buttons</Label>
+		<div className='flex flex-col gap-3 p-5'>
+			<div className='bora'>
+				<div className='flex flex-col gap-3'>
+					<Label className='text-2xl'>Switch</Label>
+					<Switch onCheckedChange={value => console.log('Checked: ', value)} />
+				</div>
+				<Separator className='mb-5 mt-5' />
+				<div className='flex flex-col gap-3'>
+					<Label className='text-2xl'>Inputs</Label>
+					<RInput placeholder='Placeholder' />
+					<RInput value='Read Only' readOnly />
+					<RInput value='Disabled' disabled />
+					<RInput copyEnabled label='Label and Icon' placeholder='Enter your blurb' />
+				</div>
+				<Separator className='mb-5 mt-5' />
+				<div className='flex flex-col gap-3'>
+					<Label className='text-2xl'>Text Area</Label>
+					<RTextarea placeholder='Placeholder' />
+					<RTextarea value='Read Only' readOnly />
+					<RTextarea value='Disabled' disabled />
+					<RTextarea copyEnabled label='Label and Icon' placeholder='Enter your blurb' />
+				</div>
+				<Separator className='mb-5 mt-5' />
+				<div className="mb-15">
+					<Label className='text-2xl'>Avatar</Label>
+					<Avatar>
+						<AvatarImage src="https://github.com/shadcn.png" />
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+				</div>
+				<Separator className='mb-5 mt-5' />
+			</div>
+			<div className='flex flex-col'>
+				<Label className='mb-5 mt-2 text-2xl'>Buttons</Label>
 				<RButton
 					onClick={() => {
 						console.log('yo');
 					}}
-					className=''
+					className='max-w-fit'
 				>
 					Click Me
 				</RButton>
-			</div>
-			<Separator className='mb-5 mt-5' />
-			<div className="mb-15">
-				<Avatar>
-					<AvatarImage src="https://github.com/shadcn.png" />
-					<AvatarFallback>CN</AvatarFallback>
-				</Avatar>
 			</div>
 			<Separator className='mb-5 mt-5' />
 			<div>Text what font am i</div>
