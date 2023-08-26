94a809dcb075b903d69e3aae65bf987f542bfcf2
Alex Young <alexyoung23j@gmail.com>
2023-08-26 19:00:29 -0400

    google sign in

--- packages/referrals-app/src/server/auth.ts
+++ packages/referrals-app/src/server/auth.ts
@@ -5,7 +5,7 @@ import {
 	type NextAuthOptions,
 	type DefaultSession,
 } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
+import GoogleProvider from 'next-auth/providers/google';
 import { env } from '~/env.mjs';
 import { prisma } from '~/server/db';
 
@@ -16,18 +16,18 @@ import { prisma } from '~/server/db';
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
 declare module 'next-auth' {
-  interface Session extends DefaultSession {
-    user: DefaultSession['user'] & {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    };
-  }
+	interface Session extends DefaultSession {
+		user: DefaultSession['user'] & {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		};
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -47,19 +47,10 @@ export const authOptions: NextAuthOptions = {
 	},
 	adapter: PrismaAdapter(prisma),
 	providers: [
-		DiscordProvider({
-			clientId: env.DISCORD_CLIENT_ID,
-			clientSecret: env.DISCORD_CLIENT_SECRET,
+		GoogleProvider({
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
 		}),
-		/**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
 	],
 };
 
@@ -69,8 +60,8 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext['req'];
-  res: GetServerSidePropsContext['res'];
+	req: GetServerSidePropsContext['req'];
+	res: GetServerSidePropsContext['res'];
 }) => {
 	return getServerSession(ctx.req, ctx.res, authOptions);
 };
