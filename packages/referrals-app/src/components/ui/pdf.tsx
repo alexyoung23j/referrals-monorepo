214bc692a6ea81eeaf121e7123b0b24b6ab16500
Bora Yuksel <borayuksel1903@gmail.com>
2023-09-02 14:42:04 -0700

    pdf scaling bug

--- packages/referrals-app/src/components/ui/pdf.tsx
+++ packages/referrals-app/src/components/ui/pdf.tsx
@@ -29,14 +29,23 @@ export const PDFRenderer = ({ fileName }: PDFRendererTypes) => {
 	const isBigScreen = useMediaQuery({ query: '(min-width: 800)' });
 	const [numPages, setNumPages] = useState<number>(0);
 	const inputRef = useRef<HTMLInputElement>(null);
+	const pdfContainerRef = useRef<HTMLDivElement>(null);
 	const [scale, setScale] = useState<number>(1);
 	const { data: { publicUrl: resumeUrl } = {} } =
 		trpc.supabase.getResume.useQuery({ fileName });
 
-	function onDocumentLoadSuccess({
-		numPages: nextNumPages,
-	}: PDFDocumentProxy): void {
+	const updateScale = () => {
+		const containerWidth = pdfContainerRef?.current?.clientWidth;
+		if (!containerWidth) {return;}
+		const pdfWidth = !isBigScreen ? 595 : 300; // Default PDF width in points (8.27 inches)
+		const calculatedScale = containerWidth / pdfWidth;
+		setScale(calculatedScale);
+	};
+
+	function onDocumentLoadSuccess(pdf: PDFDocumentProxy): void {
+		const {numPages: nextNumPages} = pdf;
 		setNumPages(nextNumPages);
+		updateScale();
 	}
 
 	const handleDownload = () => {
@@ -51,48 +60,27 @@ export const PDFRenderer = ({ fileName }: PDFRendererTypes) => {
 	};
 
 	useEffect(() => {
-		const updateScale = () => {
-			const container = document.getElementById('pdf-container');
-			const containerWidth = container && container.clientWidth;
-			if (!containerWidth) {
-				return;
-			}
-			const pdfWidth = !isBigScreen ? 595 : 300; // Default PDF width in points (8.27 inches)
-			const calculatedScale = containerWidth / pdfWidth;
-			setScale(calculatedScale);
-		};
-
-		updateScale();
-		// Update scale on window resize
-		window.addEventListener('resize', updateScale);
+		const rescalePdf = updateScale;
+		window.addEventListener('resize', rescalePdf);
 
 		return () => {
-			window.removeEventListener('resize', updateScale);
+				  window.removeEventListener('resize', rescalePdf);
 		};
-	}, [numPages, isBigScreen]);
-
+	  });
+	
 	const renderLoader = () => (
-		<div className="mx-auto flex justify-center">
-			<RSpinner size="medium" />
-		</div>
+		<div className='flex justify-center mx-auto'><RSpinner size='medium' /></div>
+	);
+
+	const renderError = () => (
+		<div className='flex justify-center mx-auto'><span>Failed to load PDF.</span></div>
 	);
 
 	return (
-		<div className="flex flex-col gap-3 p-5">
-			<div className="max-w-fit rounded border-2 border-opacity-100">
-				<Document
-					file={resumeUrl}
-					options={options}
-					loading={renderLoader}
-				>
-					<Thumbnail
-						width={250}
-						pageNumber={1}
-						pageIndex={1}
-						onClick={() =>
-							inputRef.current && inputRef.current.click()
-						}
-					/>
+		<div className='flex flex-col gap-3 p-5'>
+			<div className='max-w-fit border-textSecondary border-2 rounded border-opacity-100'>
+				<Document file={resumeUrl} options={options} loading={renderLoader} error={renderError}>
+					<Thumbnail width={250} pageNumber={1} pageIndex={1} onClick={() => inputRef.current && inputRef.current.click()} />
 				</Document>
 			</div>
 			<Dialog>
@@ -105,38 +93,29 @@ export const PDFRenderer = ({ fileName }: PDFRendererTypes) => {
 					}
 				>
 					<DialogHeader />
-					<div
-						id="pdf-container"
-						className="flex h-full max-w-full flex-col items-center overflow-hidden"
-					>
-						<ScrollArea className="flex w-full flex-col overflow-hidden">
-							<Document
+					<div ref={pdfContainerRef} id='pdf-container' className='flex flex-col items-center max-w-full h-full overflow-hidden'>
+						<ScrollArea className='flex flex-col w-full overflow-hidden'>
+							<Document 
 								file={resumeUrl}
 								options={options}
-								className="max-h-full w-full max-w-full items-center justify-center gap-3 overflow-hidden"
+								className='w-full gap-3 justify-center items-center overflow-hidden max-h-full max-w-full'
 								onLoadSuccess={onDocumentLoadSuccess}
 								loading={renderLoader}
+								error={renderError}
 							>
-								{Array.from(
-									new Array(numPages),
-									(el, index) => (
-										<div key={`page_container_${index}`}>
-											<Page
-												scale={
-													isBigScreen ? 1.25 : scale
-												}
-												className="flex w-full justify-center"
-												key={`page_${index + 1}`}
-												pageNumber={index + 1}
-												renderTextLayer={false}
-												renderAnnotationLayer={false}
-											/>
-											{index < numPages - 1 && (
-												<Separator />
-											)}
-										</div>
-									)
-								)}
+								{Array.from(new Array(numPages), (el, index) => (
+									<div key={`page_container_${index}`}>
+										<Page
+											scale={scale}
+											className='flex justify-center w-full'
+											key={`page_${index + 1}`}
+											pageNumber={index + 1}
+											renderTextLayer={false}
+											renderAnnotationLayer={false}
+										/>
+										{index < numPages - 1 && <Separator />}
+									</div>
+								))}
 							</Document>
 						</ScrollArea>
 					</div>
