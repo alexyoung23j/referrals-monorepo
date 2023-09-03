9ceb5c08cfa535a830eca8113b8bc38c73526cc4
Bora Yuksel <borayuksel1903@gmail.com>
2023-09-02 21:57:27 -0700

    add queueEmailJob mutation

--- packages/referrals-app/src/server/api/routers/email-router.ts
+++ packages/referrals-app/src/server/api/routers/email-router.ts
@@ -7,6 +7,44 @@ import { TRPCError } from '@trpc/server';
 
 // TODO: change publicProcedure to authed procedure
 export const emailRouter = createTRPCRouter({
+	queueEmailJob: publicProcedure
+		.input(
+			z.object({
+				toAddress: z.string(),
+				message: z.string().optional(),
+				referralRequestId: z.string().optional(),
+				scheduledAt: z.string().optional(),
+				emailType: z.enum([
+					'REFERRAL_REMINDER',
+					'REFERRAL_REMINDER_NOTIFICATION',
+					'REFERRAL_CONFIRMATION',
+					'REFERRAL_CONFIRMATION_NOTIFICATION',
+					'MESSAGE_FROM_REFERRER'
+				])
+			})
+		)
+		.mutation(async ({input, ctx}) => {
+			const {
+				toAddress,
+				message = '',
+				referralRequestId,
+				scheduledAt = new Date(),
+				emailType
+			} = input;
+			await ctx.prisma.emailJob.create({
+				data:
+					{
+						toAddress,
+						body: message,
+						referralRequestId,
+						attachmentUrls: [],
+						toCC: [],
+						emailType,
+						status: 'QUEUED',
+						scheduledAt
+					}
+			});
+		}),
 	createMockEmailJobEntries: publicProcedure
 		.mutation(async ({ ctx }) => {
 			await ctx.prisma.emailJob.createMany({
