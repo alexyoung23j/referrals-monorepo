715b2c1e699b8f5c299a58e50a32b8838a92a4f2
Alex Young <alexyoung23j@gmail.com>
2023-10-29 19:23:24 -0400

    beta tester page

---
+++ packages/referrals-app/src/pages/beta-testers.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function RedirectPage() {
+	const router = useRouter();
+
+	useEffect(() => {
+		window.location.href = 'https://sadk4ffy83m.typeform.com/to/Cf08waEW';
+	}, []);
+
+	return null;
+}
